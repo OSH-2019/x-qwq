aa53245b97c90e8c68a24c6dfd7e3aee67490ab2
pdlan <pengdinglan@gmail.com>
2019-07-05 23:36:01 +0800

    use xargo

--- sel4/rust/sel4-rust/build.sh
+++ sel4/rust/sel4-rust/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cargo rustc -- -C no-redzone=no -C code-model=kernel -C relocation-model=dynamic-no-pic -C link-arg="-no-pie" -C target-feature="-sse,-sse2,-sse3,-mmx,+soft-float" -C target-cpu=nehalem
+xargo build --target x86_64-sel4
